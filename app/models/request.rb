5480d158ca7a219a1cf3b6423742dd0648c2c700
Mac Logan <p13mlogan@hotmail.com>
2016-10-19 22:47:53 +0000

    Fixed table to only print one heading.  You can no longer submit entries without filling out all fields.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,11 @@
 class Request < ActiveRecord::Base
+    validates :StudentID, :presence => true
+    validates :FirstName, :presence => true
+    validates :LastName, :presence => true
+    validates :Email, :presence => true
+    validates :PULocation, :presence => true
+    validates :DOLocation, :presence => true
+    validates :PUDate, :presence => true
+    validates :PUTime, :presence => true
+
 end
