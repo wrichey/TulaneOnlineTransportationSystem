38866a0c16cb848f697d9b6348993c2328c18102
Mac Logan <p13mlogan@hotmail.com>
2016-11-15 03:05:53 +0000

    Added Javascript alert if new request form has empty fields.

--- db/migrate/20161102223309_create_users.rb
+++ db/migrate/20161102223309_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.integer :stud_id
-      t.integer :user_type
+      t.text :user_type
 
       t.timestamps null: false
     end
