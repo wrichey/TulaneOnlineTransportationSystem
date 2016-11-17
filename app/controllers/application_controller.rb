919d2029cedf1c3fd3486faa9952764b8a129a33
Sean Boyer <seanboyer11@gmail.com>
2016-11-17 08:09:34 +0000

    Added Rescue ActiveRecord

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
   end
   helper_method :current_user
 end
