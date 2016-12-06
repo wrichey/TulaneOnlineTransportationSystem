a049b93dc71d68002530cdccf0c2bdec00a55650
Mac Logan <p13mlogan@hotmail.com>
2016-12-06 05:18:08 +0000

    Students and managers can now both sort by the date the request was created

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -52,6 +52,6 @@ class RequestsController < ApplicationController
     end
   
     def sort_direction
-        %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+        %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
     end
 end
