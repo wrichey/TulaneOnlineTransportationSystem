da4d0d09d99d515f11b7179963e0b3fb8a2ee4b4
Winona Richey <wrichey@tulane.edu>
2016-12-17 23:53:51 +0000

    Add precompiled assets for Heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '1186832698029480', 'c6bb7a4763b4222ca319647b325feba5', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+      provider :facebook, '356399001390376', '2390724fef4a6f70e7e91013d19cc27a', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+      #herokuapp: '1186832698029480', 'c6bb7a4763b4222ca319647b325feba5'
+      #winonaapp: '356399001390376', '2390724fef4a6f70e7e91013d19cc27a'
 end
