4adc0c1aace87b631add150f33ff2c6113f99909
Winona Richey <wrichey@tulane.edu>
2016-12-17 20:06:31 +0000

    Edits to make CSV Download successful

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ActiveRecord::Base
+    require 'csv'
     belongs_to :user
     
     validates :PULocation, :presence => true
@@ -11,6 +12,15 @@ class Request < ActiveRecord::Base
     def not_past_date
         if self.PUDate < Date.today.to_s && self.PUDate.blank? == false
             errors.add(:date, 'not in past')
+        end
+    end
+    
+    def self.to_csv
+        CSV.generate do |csv|
+            csv << column_names
+            all.each do |request|
+                csv<< request.attributes.values_at(*column_names)
+            end
         end
     end
 end
