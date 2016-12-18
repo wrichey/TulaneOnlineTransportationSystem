886fc124087e54589b2bf3721ac0ce255550ab13
Sean <seanboyer11@gmail.com>
2016-12-18 20:00:48 +0000

    Updated Routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
+
+
+
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
@@ -12,7 +15,7 @@ config.action_mailer.smtp_settings = {
 }
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -86,4 +89,5 @@ config.action_mailer.smtp_settings = {
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
