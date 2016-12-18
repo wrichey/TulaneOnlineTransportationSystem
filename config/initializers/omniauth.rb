9e32d28fa45b8ad8356bc44056906f14e1c9b051
Winona Richey <wrichey@tulane.edu>
2016-12-18 16:57:56 +0000

    Maps show Routes; Added seeds

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,12 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '1155187911268488', 'f2b9801d7ce5c3f0bcc046a5ee23fe82', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-      #herokuapp: '1279662615439835', 'ef1368501fb7abf931d62d72aae61e05'?
+      provider :facebook, '158364724641141', 'fa9d5b252d1cb9bbff1a60c3cae741cf', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+      #winona-heroku-app: '1155187911268488', 'f2b9801d7ce5c3f0bcc046a5ee23fe82'
+      #sean-heroku-app: '1279662615439835', 'ef1368501fb7abf931d62d72aae61e05'
       #'1186832698029480', 'c6bb7a4763b4222ca319647b325feba5'
-      #winonaapp: '356399001390376', '2390724fef4a6f70e7e91013d19cc27a'
+      #winonaC9: '158364724641141', 'fa9d5b252d1cb9bbff1a60c3cae741cf'
+      
+      #Don't forget to: 
+      #rake db:drop, rake db:migrate, rake db:seed
 end
