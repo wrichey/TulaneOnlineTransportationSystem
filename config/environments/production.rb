29225e4e0eb5a8965c807a4cde0ab8e269fb12a5
Sean <seanboyer11@gmail.com>
2016-12-18 20:10:27 +0000

    Updated Routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,8 +3,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
 
-
-
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
