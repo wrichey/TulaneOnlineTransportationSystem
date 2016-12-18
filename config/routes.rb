d6f7d705cb5ee6a24ea5c328f4b0db5520ed196a
Sean <seanboyer11@gmail.com>
2016-12-18 19:14:22 +0000

    Updated Routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,9 @@ Rails.application.routes.draw do
   resources :users
   
   get '/requests' => 'requests#index'
-  get '/requests/index' => 'requests#index'
  	get '/requests/new' => 'requests#new'
   post '/requests' => 'requests#create'
-  get '/requests/:id' => 'requests#show'
+  get '/requests/:id' => 'requests#show', as: 'request'
   post '/requests/:id' => 'requests#status'
   #post '/requests/:id' => 'requests#update'
   delete '/requests/:id' => 'requests#delete'
