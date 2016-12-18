2d45436a955db573c48cde66978f3514a595d2c4
Winona Richey <wrichey@tulane.edu>
2016-12-18 21:26:34 +0000

    Create users sets user-type to student to correctly render Views and Nav bar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
     def create
         user = User.from_omniauth(env["omniauth.auth"])
         session[:user_id] = user.id
+        session[:user_type] = "Student"
         redirect_to '/requests'
     end
     
