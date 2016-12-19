28add330e0ffafbbf6c31653174ae82e0176e7e0
Winona Richey <wrichey@tulane.edu>
2016-12-19 17:32:32 +0000

    More Seeds, Footer, Students view location Names in Index

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '1279662615439835', 'ef1368501fb7abf931d62d72aae61e05', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+      provider :facebook, '1155187911268488', 'f2b9801d7ce5c3f0bcc046a5ee23fe82', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
       #winona-heroku-app: '1155187911268488', 'f2b9801d7ce5c3f0bcc046a5ee23fe82'
       #sean-heroku-app: '1279662615439835', 'ef1368501fb7abf931d62d72aae61e05'
       #'1186832698029480', 'c6bb7a4763b4222ca319647b325feba5'
